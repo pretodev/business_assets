b9ec10e1276e271a69595c6b37453abd1146165b
Silas Ribeiro <santorsilas@gmail.com>
2024-11-16 10:41:11 -0300

    Integrate Firebase services and configure Crashlytics for error reporting

--- lib/initialize.dart
+++ lib/initialize.dart
@@ -1,10 +1,39 @@
 import 'dart:async';
+import 'dart:developer';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
+
+import 'firebase_options.dart';
 
 typedef InitializeBuilder = FutureOr<Widget> Function();
 
 Future<void> initializeApp(InitializeBuilder builder) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FlutterError.onError = (details) {
+    if (kReleaseMode) {
+      FirebaseCrashlytics.instance.recordFlutterFatalError(details);
+    } else {
+      log(details.exceptionAsString(), stackTrace: details.stack);
+    }
+  };
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    if (kReleaseMode) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    } else {
+      log(error.toString(), stackTrace: stack);
+    }
+    return true;
+  };
+
   final app = await builder();
 
   runApp(app);
